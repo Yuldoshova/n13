cc31080fa53ab1ea2025cfd400e30b8c05a28d3f
Yuldoshova <ozoda8171@gmail.com>
2024-10-19 22:09:02 +0500

    lesson 3 done

---
+++ 5-month/lesson_3/lesson/gateway/src/modules/category/category.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategoryClient } from './category.client';
+
+@Injectable()
+export class CategoryService {
+
+  constructor(private categoryClient: CategoryClient
+  ) { }
+
+  create(create: CreateCategoryDto) {
+    return this.categoryClient.createOneCategory(create);
+  }
+
+  findAll() {
+    return this.categoryClient.getCategories();
+  }
+
+  findOne(id: number) {
+    return this.categoryClient.getCategory(id);
+  }
+
+  update(update: UpdateCategoryDto) {
+    return this.categoryClient.updateOneCategory(update);
+  }
+
+  remove(id: number) {
+    return this.categoryClient.removeCategory(id);
+  }
+}
---
+++ 5-month/lesson_3/lesson/gateway/src/modules/category/index.ts
@@ -0,0 +1,5 @@
+export * from './dto'
+export * from './category.client'
+export * from './category.controller'
+export * from './category.module'
+export * from './category.service'
