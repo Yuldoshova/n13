f0b0ac8be16dd33818ac1777f2e80ab52ea12ad9
Yuldoshova <ozoda8171@gmail.com>
2024-10-25 00:00:19 +0500

    deleted files

--- 5-month/lesson_9/graphql/src/modules/car/car.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CarResolver } from './car.resolver';
-import { CarService } from './car.service';
-
-@Module({
-  providers: [CarResolver, CarService],
-})
-export class CarModule {}
